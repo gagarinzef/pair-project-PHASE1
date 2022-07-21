1f798b5ddae3486b468ce01d658faf111bd221ab
gagarinzefanya <gagarinzefanya@gmail.com>
2022-07-21 18:13:45 +0700

    commit 2

--- router/router.js
+++ router/router.js
@@ -2,9 +2,10 @@ const Controller = require('../controllers/controller')
 const router = require('express').Router()
 
 router.get('/', Controller.home)
+router.get('/meme/add', Controller.addMemePage)
+router.post('/meme/add', Controller.addMeme)
 router.get('/meme/:id', Controller.memeDetail)
 router.post('/meme/:id', Controller.addComment)
-router.get('/meme/add', Controller.addMemePage)
 
 
 
